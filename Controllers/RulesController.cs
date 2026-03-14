6eb4ab105cdd5f4923c1a03676fa15667f12ffe5
Swastik-IFPV <insidefpvrd6@gmail.com>
2026-03-14 14:53:08 +0530

    Rules added

---
+++ Controllers/RulesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccommodationManagement.Controllers
+{
+    public class RulesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
